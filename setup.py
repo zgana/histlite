cf5a1acc3246933974a8201f90e6a66bd5fc05d9
Mike Richman <mike.d.richman@gmail.com>
2019-06-12 21:50:32 -0400

    preparing setup.py ahead of initial release

--- setup.py
+++ setup.py
@@ -4,16 +4,21 @@ from setuptools import setup
 import sys
 import setuptools
 
-__version__ = '0.9.0'
+__version__ = '0.9.1'
 
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 setup(
     name='histlite',
-    packages = ['histlite'],
     version=__version__,
     author='Mike Richman',
     author_email='mike.d.richman@gmail.com',
+    packages = ['histlite'],
     description='A somewhat "lite" histogram library',
-    long_description='',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/zgana/histlite',
     install_requires=['numpy', 'scipy', 'matplotlib'],
 )
